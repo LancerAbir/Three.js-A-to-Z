3d1769b0e0da143eee28d77bac9ed9fca7495a2b
lancer abir <you@example.com>
2021-06-26 00:37:27 +0600

    first commit

---
+++ project_one/main.js
@@ -0,0 +1,10 @@
+// import * as THREE from "three";
+// const scene = new THREE.Scene();
+// console.log(scene);
+
+// Find the latest version by visiting https://cdn.skypack.dev/three.
+import * as THREE from "https://unpkg.com/three@0.129.0/build/three.module.js";
+
+const scene = new THREE.Scene();
+
+console.log(scene);
