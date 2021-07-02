ea5fe4c1185b128333bbcbd09c3c4cf2bcfd9da7
lancer abir <you@example.com>
2021-07-02 13:07:25 +0600

    ADS

---
+++ basic/rules.js
@@ -0,0 +1,24 @@
+Three JS
+---------------
+
+
+// Download
+-------------
+three.js
+three-min.js
+
+// Setup
+
+// Create Scene
+const scene = new THREE.Scene();
+
+// camera add
+const camera = new THREE.PerspectiveCamera("field of view", "aspect", "near", "far");
+
+/*
+fov (Float) — Camera frustum vertical field of view.
+aspect (Float) — Camera frustum aspect ratio. 
+near (Float) — Camera frustum near plane.
+far (Float) — Camera frustum far plane.
+*/
+
